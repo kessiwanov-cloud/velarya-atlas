720a931fa60da3c0a9cfc2aa0508a6131d9b9368
kessiwanov-cloud <kessiwanov@gmail.com>
2026-07-09 19:00:17 +0200

    Update data.js

--- data.js
+++ data.js
@@ -1,27 +1,99 @@
 // x und y sind Prozentwerte. Dadurch passen die Marker auch bei 2K, 4K oder 8K.
 let CITIES = [
   {
-    name: "Sylvera",
-    area: "Hof des Mondes",
-    icon: "🌙",
-    x: 44.344,
-    y: 5.944,
-    discord: "https://discord.com/channels/1514984333344112811/1518861674579230800"
+    "name": "Sylvera",
+    "area": "Hof des Mondes",
+    "icon": "🌙",
+    "x": 44.344,
+    "y": 5.944,
+    "discord": "https://discord.com/channels/1514984333344112811/1518861674579230800"
   },
   {
-    name: "Ebonvale",
-    area: "Hof der Nacht",
-    icon: "🌑",
-    x: 64.380,
-    y: 5.697,
-    discord: "https://discord.com/channels/1514984333344112811/1518860134129078395"
+    "name": "Ebonvale",
+    "area": "Hof der Nacht",
+    "icon": "🌑",
+    "x": 64.38,
+    "y": 5.697,
+    "discord": "https://discord.com/channels/1514984333344112811/1518860134129078395"
   },
   {
-    name: "Amaranthia",
-    area: "Hof der Vier Jahreszeiten",
-    icon: "🌸",
-    x: 63.145,
-    y: 26.627,
-    discord: "https://discord.com/channels/1514984333344112811/1518888878390382632"
+    "name": "Amaranthia",
+    "area": "Hof der Vier Jahreszeiten",
+    "icon": "🌸",
+    "x": 63.145,
+    "y": 26.627,
+    "discord": "https://discord.com/channels/1514984333344112811/1518888878390382632"
+  },
+  {
+    "name": "Auroria",
+    "area": "Haftenstadt der Lichfae",
+    "icon": "📍",
+    "x": 97.438,
+    "y": 19.138,
+    "discord": "https://discord.com/channels/1514984333344112811/1524766439066566818"
+  },
+  {
+    "name": "Sonnenquell",
+    "area": "",
+    "icon": "📍",
+    "x": 94.378,
+    "y": 19.057,
+    "discord": "https://discord.com/channels/1514984333344112811/1524765994126671922"
+  },
+  {
+    "name": "Nebelwacht",
+    "area": "",
+    "icon": "📍",
+    "x": 87.229,
+    "y": 22.409,
+    "discord": "https://discord.com/channels/1514984333344112811/1524765467334541442"
+  },
+  {
+    "name": "Dämmerfels",
+    "area": "",
+    "icon": "📍",
+    "x": 73.778,
+    "y": 19.057,
+    "discord": "https://discord.com/channels/1514984333344112811/1524764970200469504"
+  },
+  {
+    "name": "Aurelith",
+    "area": "",
+    "icon": "📍",
+    "x": 80.14,
+    "y": 11.988,
+    "discord": "https://discord.com/channels/1514984333344112811/1524749354013949982"
+  },
+  {
+    "name": "Vesperhain",
+    "area": "",
+    "icon": "📍",
+    "x": 75.233,
+    "y": 10.696,
+    "discord": "https://discord.com/channels/1514984333344112811/1524748910218842203"
+  },
+  {
+    "name": "Knochenweber Dorf",
+    "area": "",
+    "icon": "📍",
+    "x": 40.813,
+    "y": 9.271,
+    "discord": "https://discord.com/channels/1514984333344112811/1524745804617748620"
+  },
+  {
+    "name": "Schattenhain",
+    "area": "",
+    "icon": "📍",
+    "x": 68.497,
+    "y": 6.996,
+    "discord": "https://discord.com/channels/1514984333344112811/1524745461481865247"
+  },
+  {
+    "name": "Nyxmoor",
+    "area": "Hafenstadt der Nachtfae",
+    "icon": "📍",
+    "x": 68.043,
+    "y": 2.551,
+    "discord": "https://discord.com/channels/1514984333344112811/1524745008849096754"
   }
 ];
